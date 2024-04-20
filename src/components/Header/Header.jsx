559de50d7ae50937b8f4da6fd585526fca698c50
kietNguyen <nguyentuankietmd2003@gmail.com>
2024-04-20 22:18:49 +0700

    update responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,21 +6,25 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
   const { infoUser } = useSelector((state) => state.userReducer);
+
   const renderUserNav = () => {
     if (infoUser) {
-      //if logined => Name & logout button
+      //if logged in => Name & logout button
       return <UserNavLogin />;
     } else {
-      // If not login => login/register button
+      // If not logged in => login/register button
       return <UserNavLogOut />;
     }
   };
+
   return (
     <div className="bg-gray-500 py-3 sticky top-0 z-40">
-      <div className="container max-w-7xl mx-auto flex justify-between items-center">
-        <NavLink to={"/"} className="text-white font-medium text-2xl">
-            <span className="font-bold">Movie</span>
+      <div className="container mx-auto px-4 lg:px-0 flex justify-between items-center">
+        <NavLink to={"/"} className="text-white font-medium text-2xl flex items-center">
+          <span className="font-bold">Movie</span>
+          
             <img src="https://cdn.pixabay.com/photo/2017/01/31/23/34/vintage-2028217_640.png" alt="" className="h-8 w-14 inline"/> 
+          
         </NavLink>
         <div className="text-white">{renderUserNav()}</div>
       </div>
